47cc19f8b365cf579da3f0f7814d94916110e03f
robetto <robertobettoni@gmail.com>
2022-03-15 11:15:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import Game from "./components/Game";
+
+function App() {
+  const darkHandler = (dark) => {
+    if (dark) document.documentElement.classList.add("dark");
+    else document.documentElement.classList.remove("dark");
+  };
+  
+  return (
+    <div className={"app dark:bg-zinc-800"}>
+      <Game darkness={darkHandler} />
+    </div>
+  );
+}
+
+export default App;
